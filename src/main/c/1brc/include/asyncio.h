b0dd78b7089eefb07f006e3da647ffe65deb6a83
isaiahp.8910 <isaiahp.8910@gmail.com>
2025-10-31 18:15:32 +0000

    clean up api asyncio.h

--- src/main/c/1brc/include/asyncio.h
+++ src/main/c/1brc/include/asyncio.h
@@ -26,12 +26,14 @@ int async_reader_free(struct async_reader_t *reader);
 /**
  *
  * @param reader
- * @return number of block received
+ * @return number of block requests completed and received from kernel
  */
 int async_reader_poll(struct async_reader_t *reader);
 
 /**
- *
+ * blocks can be completed out of order,
+ * this method will ensure block are return in order
+ * return the next block that ready to be read
  * @param reader
  * @param size
  * @return ptr to buffer with data for next block that is ready for reading , NULL and size == 0 if next block in order is not yet ready
@@ -39,7 +41,7 @@ int async_reader_poll(struct async_reader_t *reader);
 const char* async_reader_next_ready(struct async_reader_t *reader, __uint64_t* size);
 
 /**
- * Notify reader blk is consumed,
+ * Notify reader blk is consumed, notifies kernel to reuse the block for subsequent reads 
  * @param r
  * @param size
  * @return
